27da12835595fa257aa608d3afc17eb535adf610
pyc <18165814062pyc@gmail.com>
2024-05-11 00:16:35 +0800

    hi idea

---
+++ src/main/java/com/pyc/git/controller/HelloworldController.java
@@ -0,0 +1,9 @@
+package com.pyc.git.controller;
+
+import java.sql.SQLOutput;
+
+public class HelloworldController {
+    public static void helloWorld(){
+        System.out.println("Hello Controller");
+    }
+}
